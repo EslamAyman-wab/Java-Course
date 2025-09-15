85dcb7007ef97a4ff01a395d5795c1c23b79eecf
EslamAyman-wab <eslamayman2372002ii@gmail.com>
2025-09-15 16:28:22 +0300

    test

--- src/B_Coding_Exercise/Ex_05.java
+++ src/B_Coding_Exercise/Ex_05.java
@@ -3,7 +3,20 @@ package B_Coding_Exercise;
 public class Ex_05 {
     //Implementing A Precise Leap Year Calculator To Enhance Conditional Reasoning
     public static void main(String[] args) {
+        // Test case 1: Common leap year (divisible by 4)
+        System.out.println("Year 2024 is leap year: " + isLeapYear(2024));
 
+        // Test case 2: Century year (divisible by 100 but not 400)
+        System.out.println("Year 1900 is leap year: " + isLeapYear(1900));
+
+        // Test case 3: Century leap year (divisible by 400)
+        System.out.println("Year 2000 is leap year: " + isLeapYear(2000));
+
+        // Test case 4: Common year
+        System.out.println("Year 2023 is leap year: " + isLeapYear(2023));
+
+        // Test case 5: Invalid year
+        System.out.println("Year 10000 is leap year: " + isLeapYear(10000));
     }
 
     public static boolean isLeapYear(int year) {
