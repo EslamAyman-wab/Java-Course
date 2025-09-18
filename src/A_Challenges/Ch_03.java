c1388b0beab2faad101960f00dca4ca4b36557af
EslamAyman-wab <eslamayman2372002ii@gmail.com>
2025-09-18 20:32:53 +0300

    Add new challenge classes and XML configuration for Java exercises

---
+++ src/A_Challenges/Ch_03.java
@@ -0,0 +1,30 @@
+package A_Challenges;
+
+public class Ch_03 {
+    public static void main(String[] args) {
+
+        char nato = 'A';
+
+        switch (nato) {
+
+            case 'A':
+                System.out.println("Able");
+                break;
+            case 'B':
+                System.out.println("Baker");
+            case 'C':
+                System.out.println("Charlie");
+                break;
+            case 'D':
+                System.out.println("Dog");
+                break;
+            case 'E':
+                System.out.println("Easy");
+                break;
+            default:
+                System.out.printf("invalid = %s%n", nato);
+        }
+
+
+    }
+}
