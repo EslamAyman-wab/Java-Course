eab48d60a51c1716fa8c064eb407450be4e72977
EslamAyman-wab <eslamayman2372002ii@gmail.com>
2025-10-20 10:05:50 +0300

    بيبي

--- src/A_Challenges/B_Ch/Ch_13/Main.java
+++ src/A_Challenges/B_Ch/Ch_13/Main.java
@@ -3,5 +3,12 @@ package A_Challenges.B_Ch.Ch_13;
 public class Main {
     public static void main(String[] args) {
 
+        Customer customer1 = new Customer("Eslam", 20_000,"eslam.com");
+        
+        Customer customer2 = new Customer("Eslam","eslam.com");
+
+        Customer customer3 = new Customer();
+
+        System.out.println(customer1 +"/n c");
     }
 }
