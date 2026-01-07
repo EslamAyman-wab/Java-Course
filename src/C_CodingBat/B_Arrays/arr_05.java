752fea6e824f9fd340eb3eed3999d573e01a13ed
EslamAyman-wab <eslamayman2372002ii@gmail.com>
2026-01-07 16:22:58 +0200

    Add arr_05 and arr_06 classes with methods for summing elements and checking for double 2s or 3s

---
+++ src/C_CodingBat/B_Arrays/arr_05.java
@@ -0,0 +1,11 @@
+package C_CodingBat.B_Arrays;
+
+public class arr_05 {
+    public static void main(String[] args) {
+
+    }
+
+    public int sum3(int[] nums) {
+        return nums[0] + nums[1] + nums[2];
+    }
+}
