47ca13c5458facc9a0c2a86f3adea2ffccda424f
eslam <eslamayman2372002ii@gmail.com>
2026-02-25 21:33:06 +0200

    Add Account and Rectangle classes with constructors and Main class for demonstration day 4 of coding

---
+++ src/E_W3resource/E_oop/A_constructor/Ch_06/Account.java
@@ -0,0 +1,4 @@
+package E_W3resource.E_oop.A_constructor.Ch_06;
+
+public class Account {
+}
---
+++ src/E_W3resource/E_oop/A_constructor/Ch_06/Main.java
@@ -0,0 +1,7 @@
+package E_W3resource.E_oop.A_constructor.Ch_06;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
