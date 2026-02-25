c39c15c644ef4b9bbdd5cb31fbd2b582721b6b4d
eslam <eslamayman2372002ii@gmail.com>
2026-02-25 22:16:02 +0200

    Add Account and Rectangle classes with constructors and Main class for demonstration day 4 of coding

--- src/E_W3resource/E_oop/A_constructor/Ch_06/Account.java
+++ src/E_W3resource/E_oop/A_constructor/Ch_06/Account.java
@@ -1,4 +1,37 @@
 package E_W3resource.E_oop.A_constructor.Ch_06;
 
 public class Account {
+
+    private String accountNumber;
+    private double balance;
+
+    public Account(String accountNumber, double balance) {
+
+        if (accountNumber == null || accountNumber.isEmpty()) {
+            throw new IllegalArgumentException("Account number cannot be null or empty");
+        }
+        this.accountNumber = accountNumber;
+
+        if (balance < 0.0) {
+            throw new IllegalArgumentException("Balance cannot be negative");
+        }
+        this.balance = balance;
+
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
 }
