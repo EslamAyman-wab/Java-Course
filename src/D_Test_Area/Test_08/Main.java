3b4f0d3c3a9694281141409904dc282c8d1eebfe
eslam <eslamayman2372002ii@gmail.com>
2026-03-02 21:52:55 +0200

    Add Department and Employee classes with constructors and demonstrate in Main class day 7 of coding

---
+++ src/D_Test_Area/Test_08/Main.java
@@ -0,0 +1,12 @@
+package D_Test_Area.Test_08;
+
+public class Main {
+    public static void main(String[] args) {
+        Personalinformation personalinformation = new Personalinformation("John", "A.", "Doe", "O+", "123456789", "American", 1990);
+        Department department = new Department(101, "Engineering");
+        Employee employee = new Employee("John", "A.", "Doe", "O+", "123456789", "American", 1990, 1, 75000.0f, personalinformation);
+
+        System.out.println(employee);
+
+    }
+}
---
+++ src/D_Test_Area/Test_08/Personalinformation.java
@@ -0,0 +1,38 @@
+package D_Test_Area.Test_08;
+
+public class Personalinformation {
+    private String firstName;
+    private String middleName;
+    private String lastName;
+    private String bloodGroup;
+    private String accountNumber;
+    private String nationality;
+    private int yearOfBirth;
+
+    public Personalinformation() {
+    }
+
+    public Personalinformation(String firstName, String middleName, String lastName, String bloodGroup, String accountNumber, String nationality, int yearOfBirth) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.bloodGroup = bloodGroup;
+        this.accountNumber = accountNumber;
+        this.nationality = nationality;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Personalinformation{" +
+                "firstName='" + firstName + '\'' +
+                ", middleName='" + middleName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", bloodGroup='" + bloodGroup + '\'' +
+                ", accountNumber='" + accountNumber + '\'' +
+                ", nationality='" + nationality + '\'' +
+                ", yearOfBirth=" + yearOfBirth +
+                '}';
+    }
+}
