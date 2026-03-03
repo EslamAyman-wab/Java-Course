1568b9617914b8263d4e30f760d7be4953e9479c
eslam <eslamayman2372002ii@gmail.com>
2026-03-03 22:20:54 +0200

    Add abstract Employee class with Manager and Programmer implementations for salary calculation day 8 of coding

---
+++ src/B_Exercises/B_W3resource/E_oop/H_Polymorphism/Ch_04/Employee.java
@@ -0,0 +1,38 @@
+package B_Exercises.B_W3resource.E_oop.H_Polymorphism.Ch_04;
+
+public abstract class Employee {
+    public abstract double calculateSalary();
+
+    ;
+}
+
+class Manager extends Employee {
+    private double baseSalary;
+    private double bonus;
+
+    public Manager(double baseSalary, double bonus) {
+        this.baseSalary = baseSalary;
+        this.bonus = bonus;
+    }
+
+    @Override
+    public double calculateSalary() {
+
+        return baseSalary +(baseSalary*bonus);
+    }
+}
+
+class Programmer extends Employee {
+    private double hourlyRate;
+    private double workedHours;
+
+    public Programmer(double hourlyRate, double workedHours) {
+        this.hourlyRate = hourlyRate;
+        this.workedHours = workedHours;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return hourlyRate*workedHours;
+    }
+}
