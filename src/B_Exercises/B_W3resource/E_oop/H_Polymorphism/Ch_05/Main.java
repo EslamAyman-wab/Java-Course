73decce47d5891d6393e1d29fa09459690d7691f
eslam <eslamayman2372002ii@gmail.com>
2026-03-04 21:48:07 +0200

    Refactor Employee class hierarchy by adding additional methods for salary calculation and enhancing Manager and Programmer implementations

---
+++ src/B_Exercises/B_W3resource/E_oop/H_Polymorphism/Ch_05/Main.java
@@ -0,0 +1,10 @@
+package B_Exercises.B_W3resource.E_oop.H_Polymorphism.Ch_05;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("this is a test");
+
+    }
+}
