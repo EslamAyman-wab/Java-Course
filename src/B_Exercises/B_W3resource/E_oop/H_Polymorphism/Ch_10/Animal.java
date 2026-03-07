ccfe63b4b0d3b371081fee0844eb6d772da9ac65
eslam <eslamayman2372002ii@gmail.com>
2026-03-07 22:08:06 +0200

    Add Vehicle class hierarchy with Car and Motorcycle implementations for engine start and stop functionality day 12 of coding

---
+++ src/B_Exercises/B_W3resource/E_oop/H_Polymorphism/Ch_10/Animal.java
@@ -0,0 +1,36 @@
+package B_Exercises.B_W3resource.E_oop.H_Polymorphism.Ch_10;
+
+public abstract class Animal {
+
+    public abstract void eat();
+    public abstract void sound();
+
+}
+
+class Lion extends Animal {
+    public void eat() {
+        System.out.println("Eating zepra");
+    }
+    public void sound() {
+        System.out.println("WOOOOOO");
+    }
+}
+
+class Tiger extends Animal {
+    public void eat() {
+        System.out.println("Eating cows");
+    }
+
+    public void sound() {
+        System.out.println("Hoooooo");
+    }
+
+}
+class Panther extends Animal {
+    public void eat() {
+        System.out.println("Eating sheeps");
+    }
+    public void sound() {
+        System.out.println("Meoooo");
+    }
+}
