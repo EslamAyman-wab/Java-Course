93eda5d00803692901a0b20ad03313f8de146c74
eslam <eslamayman2372002ii@gmail.com>
2026-03-09 21:40:57 +0200

    day 13 of coding

--- src/D_Test_Area/Test_09/Main.java
+++ src/D_Test_Area/Test_09/Main.java
@@ -1,10 +1,12 @@
 package D_Test_Area.Test_09;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        int arr[] = {4,55,56,-3,52,32,19};
+        int[] arr = {4,55,56,-3,52,32,19};
         int min = arr[0];
-        int max = arr[0];
+        int max = arr[0];                                                                                          
         for (int i : arr) {
             if(i < min){
                 min = i;
@@ -12,8 +14,8 @@ public class Main {
             if(i > max){
                 max = i;
             }
-
         }
+        Arrays.sort(arr);
         System.out.println("The minimum value is " + min);
         System.out.println("The maximum value is " + max);
     }
---
+++ src/D_Test_Area/Test_10/Main.java
@@ -0,0 +1,14 @@
+package D_Test_Area.Test_10;
+
+public class Main {
+     static void print (int []x){
+         for (int i=0;i<x.length;i++){
+             System.out.println(x[i]);
+         }
+     }
+    public static void main(String[] args) {
+         int arr[] ={1,2,4,5,6,67,74};
+         print(arr);
+
+    }
+}
