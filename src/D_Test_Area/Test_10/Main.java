d31c920b4ccfab92bde5974b71bc58942dcfd3b6
eslam <eslamayman2372002ii@gmail.com>
2026-03-10 22:11:32 +0200

    day 14 of coding

--- src/D_Test_Area/Test_10/Main.java
+++ src/D_Test_Area/Test_10/Main.java
@@ -2,17 +2,20 @@ package D_Test_Area.Test_10;
 
 public class Main {
     public static void main(String[] args) {
-        int [][] arr ={
-                {1,4,5,7},
-                {3,4,6,23},
-                {34,56,73}
-        };
-
-        for (int row = 0; row < arr.length; row++) {
-            for (int cal = 0; cal < arr[row].length; cal++) {
-                System.out.print(arr[row][cal] + " ");
-            }
-            System.out.println();
-        }
+//        int [][] arr ={
+//                {1,4,5,7},
+//                {3,4,6,23},
+//                {34,56,73}
+//        };
+//
+//        for (int row = 0; row < arr.length; row++) {
+//            for (int cal = 0; cal < arr[row].length; cal++) {
+//                System.out.print(arr[row][cal] + " ");
+//            }
+//            System.out.println();
+//        }
+        String s1 = "ABCD";
+        String s2 = "ABCd";
+        System.out.println(s1.compareTo(s2));
     }
 }
