172349a06e90d85117135f6bc304630224b2a944
eslam <eslamayman2372002ii@gmail.com>
2026-03-10 22:05:44 +0200

    day 14 of coding

--- src/D_Test_Area/Test_10/Main.java
+++ src/D_Test_Area/Test_10/Main.java
@@ -1,14 +1,18 @@
 package D_Test_Area.Test_10;
 
 public class Main {
-     static void print (int []x){
-         for (int i=0;i<x.length;i++){
-             System.out.println(x[i]);
-         }
-     }
     public static void main(String[] args) {
-         int arr[] ={1,2,4,5,6,67,74};
-         print(arr);
+        int [][] arr ={
+                {1,4,5,7},
+                {3,4,6,23},
+                {34,56,73}
+        };
 
+        for (int row = 0; row < arr.length; row++) {
+            for (int cal = 0; cal < arr[row].length; cal++) {
+                System.out.print(arr[row][cal] + " ");
+            }
+            System.out.println();
+        }
     }
 }
