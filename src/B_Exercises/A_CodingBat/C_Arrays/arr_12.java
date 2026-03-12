45ae6bc6760c5a5b2a1f7599278f58cc0b255590
eslam <eslamayman2372002ii@gmail.com>
2026-03-12 22:00:30 +0200

    i have solve a challenge on codeben day 16 of coding

--- src/B_Exercises/A_CodingBat/C_Arrays/arr_12.java
+++ src/B_Exercises/A_CodingBat/C_Arrays/arr_12.java
@@ -1,7 +1,26 @@
 package B_Exercises.A_CodingBat.C_Arrays;
 
+
+import java.util.Arrays;
+
 public class arr_12 {
     public static void main(String[] args) {
+
+        int [] a = {1, 2};
+        int [] b = {3, 4};
+        System.out.println(Arrays.toString(biggerTwo(a, b)));
+
+    }
+
+    public static int[] biggerTwo(int[] a, int[] b) {
+        int sumA = a[0] + a[1];
+        int sumB = b[0] + b[1];
+
+        if (sumA >= sumB) {
+            return a;
+        } else {
+            return b;
+        }
 
     }
 }
