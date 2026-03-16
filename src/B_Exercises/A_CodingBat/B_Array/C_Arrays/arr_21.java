f17b407821cc9604b4369691b613a85611744529
eslam <eslamayman2372002ii@gmail.com>
2026-03-16 22:21:12 +0200

    some changes on folders and today is day 19 off coding java :)

--- src/B_Exercises/A_CodingBat/B_Array/C_Arrays/arr_21.java
+++ src/B_Exercises/A_CodingBat/B_Array/C_Arrays/arr_21.java
@@ -4,4 +4,10 @@ public class arr_21 {
     public static void main(String[] args) {
 
     }
+    public int[] makeLast(int[] nums) {
+        int newLength = nums.length * 2;
+        int[] newNums = new int[newLength];
+        newNums[newLength - 1] = nums[nums.length - 1];
+        return newNums;
+    }
 }
