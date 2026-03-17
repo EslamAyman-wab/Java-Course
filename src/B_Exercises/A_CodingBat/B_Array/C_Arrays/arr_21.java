da7f1ef9989d3d1d6292f50dc521d2a80debf48e
eslam <eslamayman2372002ii@gmail.com>
2026-03-17 20:27:21 +0200

    some changes on folders and today is day 20 off coding java :)

--- src/B_Exercises/A_CodingBat/B_Array/C_Arrays/arr_21.java
+++ src/B_Exercises/A_CodingBat/B_Array/C_Arrays/arr_21.java
@@ -2,7 +2,19 @@ package B_Exercises.A_CodingBat.B_Array.C_Arrays;
 
 public class arr_21 {
     public static void main(String[] args) {
+        arr_21 obj = new arr_21();
 
+        // Test case 1
+        int[] result1 = obj.makeLast(new int[]{4, 5, 6});
+        System.out.println("Test 1: [4, 5, 6] -> " + java.util.Arrays.toString(result1));
+
+        // Test case 2
+        int[] result2 = obj.makeLast(new int[]{1, 2});
+        System.out.println("Test 2: [1, 2] -> " + java.util.Arrays.toString(result2));
+
+        // Test case 3
+        int[] result3 = obj.makeLast(new int[]{3});
+        System.out.println("Test 3: [3] -> " + java.util.Arrays.toString(result3));
     }
     public int[] makeLast(int[] nums) {
         int newLength = nums.length * 2;
