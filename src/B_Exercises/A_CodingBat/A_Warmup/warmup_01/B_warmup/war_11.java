c63f8a6194c2333027c9c689e3ef188701a34977
eslam <eslamayman2372002ii@gmail.com>
2026-03-26 17:11:35 +0200

    interface day 25 of coding part 2

--- src/B_Exercises/A_CodingBat/A_Warmup/warmup_01/B_warmup/war_11.java
+++ src/B_Exercises/A_CodingBat/A_Warmup/warmup_01/B_warmup/war_11.java
@@ -1,5 +1,26 @@
 package B_Exercises.A_CodingBat.A_Warmup.warmup_01.B_warmup;
 
 public class war_11 {
-    
+
+    public static void main(String[] args) {
+
+
+    }
+
+    boolean isInRange(int value, int min, int max) {
+
+        return (value >= min && value <= max);
+
+    }
+
+    public boolean in3050(int a, int b) {
+
+//        return ((a >= 30 && a <= 40) && (b >= 30 && b <= 40)) || ((a >= 40 && a <= 50) && (b >= 40 && b <= 50));
+
+        return isInRange(a,30,40) && isInRange(b,30,40) ||
+                isInRange(b,40,50) && isInRange(b,40,50);
+
+    }
+
+
 }
---
+++ src/B_Exercises/A_CodingBat/A_Warmup/warmup_01/B_warmup/war_12.java
@@ -0,0 +1,7 @@
+package B_Exercises.A_CodingBat.A_Warmup.warmup_01.B_warmup;
+
+public class war_12 {
+    public static void main(String[] args) {
+        
+    }
+}
