995959aaab9fc0f1d89d7d75eb8e91404cf571d8
eslam <eslamayman2372002ii@gmail.com>
2026-03-30 17:14:23 +0200

    Solving challenges day 28 of coding part 1

--- src/B_Exercises/A_CodingBat/A_Warmup/warmup_01/B_warmup/war_12.java
+++ src/B_Exercises/A_CodingBat/A_Warmup/warmup_01/B_warmup/war_12.java
@@ -2,6 +2,21 @@ package B_Exercises.A_CodingBat.A_Warmup.warmup_01.B_warmup;
 
 public class war_12 {
     public static void main(String[] args) {
-        
+
     }
+
+    public int close10(int a, int b) {
+
+        int x = Math.abs(a - 10);
+        int y = Math.abs(b - 10);
+
+        if (x > y) {
+            return b;
+        }
+        if (y > x) {
+            return a;
+        }
+        return 0;
+    }
+
 }
