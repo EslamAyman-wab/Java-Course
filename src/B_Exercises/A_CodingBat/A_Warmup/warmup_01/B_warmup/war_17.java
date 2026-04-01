7973cfb904a23e94ff9d5f870c2b2de865199f71
eslam <eslamayman2372002ii@gmail.com>
2026-04-01 16:55:31 +0200

    Solving challenges day 30 of coding part 2

---
+++ src/B_Exercises/A_CodingBat/A_Warmup/warmup_01/B_warmup/war_17.java
@@ -0,0 +1,13 @@
+package B_Exercises.A_CodingBat.A_Warmup.warmup_01.B_warmup;
+
+public class war_17 {
+    public static void main(String[] args) {
+
+
+    }
+
+    public String backAround(String str) {
+        char last = str.charAt(str.length() - 1);
+        return "" + last + str + last;
+    }
+}
