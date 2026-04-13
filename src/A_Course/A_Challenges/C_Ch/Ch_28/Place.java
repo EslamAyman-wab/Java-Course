0d0f7136458bca3ae8d307e684ee8579f50f40ec
eslam <eslamayman2372002ii@gmail.com>
2026-04-13 23:28:44 +0200

    Add Main and Place classes for managing a list of places

---
+++ src/A_Course/A_Challenges/C_Ch/Ch_28/Place.java
@@ -0,0 +1,19 @@
+package A_Course.A_Challenges.C_Ch.Ch_28;
+
+public class Place {
+    private String name;
+    private int length;
+
+    public Place(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getlength() {
+        return length;
+    }
+}
