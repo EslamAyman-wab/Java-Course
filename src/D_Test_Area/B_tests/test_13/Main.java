bb46a630fcfa5d127ab8fab071888f17c395d4a4
eslam <eslamayman2372002ii@gmail.com>
2026-04-20 11:08:21 +0200

    Refactor Printer instantiation for consistency in Main class

--- src/D_Test_Area/B_tests/test_13/Main.java
+++ src/D_Test_Area/B_tests/test_13/Main.java
@@ -2,13 +2,13 @@ package D_Test_Area.B_tests.test_13;
 
 public class Main {
     public static void main(String[] args) {
-        Printer <Integer> printer1 = new Printer<>(23);
+        Printer<Integer> printer1 = new Printer<>(23);
         printer1.print();
 
-
-        Printer <Double> printer2 = new Printer<>(23.33);
+        Printer<Double> printer2 = new Printer<>(23.33);
         printer2.print();
 
+        System.out.println("this is a test");
 
     }
 }
