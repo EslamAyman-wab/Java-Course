af73b6f6fff16ee1955d69f49162959564b2eb6b
eslam <eslamayman2372002ii@gmail.com>
2026-07-12 15:11:16 +0300

    Clean up

--- Chapter_06/src/Challenge/Ch_01/Main.java
+++ Chapter_06/src/Challenge/Ch_01/Main.java
@@ -3,7 +3,7 @@ package Challenge.Ch_01;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("this is a tesgt ");
+        System.out.println("this is a test ");
 
     }
 }
